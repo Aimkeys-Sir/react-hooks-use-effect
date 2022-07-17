37dff061cfc871fcdf2100dc61b2617894d91175
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-17 11:46:10 +0300

    commit tree 6f66bbbdf4b48ee78ae978faf67c779c7ab15d25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [text, setText] = useState("")
+  const [count, setCount] = useState(5)
   useEffect(() => {
+    setTimeout(()=>setCount(0),5000)
     console.log("useEffect called");
-  });
+  },[]);
 
   console.log("Component rendering");
-
-  return <button>Click Me</button>;
+  console.log(count)
+  return (
+    <div>
+      <DogPics/>
+      <button onClick={() => setCount(count => count += 1)}>{`I have been clicked ${count} times`}</button>
+      <input placeholder="Type away..." type={"text"} value={text}
+        onChange={e => setText(e.target.value)} />
+    </div>
+  )
 }
 
 export default App;
